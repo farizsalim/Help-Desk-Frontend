484edaf4787a1c04eb8656a631ceec24edab8eaf
farizsalim <farizsalim10@yahoo.co.id>
2026-02-25 14:04:03 +0800

    version 1.0

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.56.50',  // Listen on all network interfaces
+    port: 5173,       // Default Vite port
+    strictPort: true  // Fail if port is already in use
+  }
 })
