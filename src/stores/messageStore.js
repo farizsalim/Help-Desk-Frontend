7403b406e4c8ebff12f3bb247a9bbe47f3b54c70
farizsalim <farizsalim10@yahoo.co.id>
2026-03-04 16:52:07 +0800

    testing

--- src/stores/messageStore.js
+++ src/stores/messageStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import axios from 'axios'
 import { useAuthStore } from './authStore'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_API_URL
 
 export const useMessageStore = create((set, get) => ({
   // State
